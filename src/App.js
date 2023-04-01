ac1d9f4593ddcadfb75ce829c56f3c6444ee5cbc
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-04-01 12:26:04 -0400

    Reformat with new Prettier settings

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
 function List({ notes }) {
     console.log(notes);
     return (
-        <ul className="noteList">
+        <ul className='noteList'>
             {notes.map((note, index) => (
-                <li key={index} className="note">{`${note.content}`}</li>
+                <li
+                    key={index}
+                    className='note'
+                >
+                    {`${note.content}`}
+                </li>
             ))}
         </ul>
     );
 }
 
 function Form({ addNote }) {
-    const [content, setContent] = useState("");
+    const [content, setContent] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!content) {
             return;
         }
-        console.log("a new item is added...", content);
+        console.log('a new item is added...', content);
         addNote({
             content: content,
         });
 
-        setContent("")
+        setContent('');
     };
 
     return (
-        <form id="newNoteForm" onSubmit={handleSubmit}>
-            <input id="newNoteInput" className="note" type="text" value={content} onChange={(e) => setContent(e.target.value)} />
+        <form
+            id='newNoteForm'
+            onSubmit={handleSubmit}
+        >
+            <input
+                id='newNoteInput'
+                className='note'
+                type='text'
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+            />
         </form>
     );
 }
 
 function App() {
-    const [notes, setNotes] = useState([{ content: "Start my to-do list" }]);
+    const [notes, setNotes] = useState([{ content: 'Start my to-do list' }]);
 
     function handleAddNote(newNote) {
         const updatedNotes = [...notes, newNote];
@@ -44,19 +58,20 @@ function App() {
     }
 
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className='App'>
+            <header className='App-header'>
                 <h1>Todo List & Notes</h1>
 
-                <button id="addNote" onClick={handleAddNote}>
-                    {"+"}
+                <button
+                    id='addNote'
+                    onClick={handleAddNote}
+                >
+                    {'+'}
                 </button>
 
                 <Form addNote={handleAddNote} />
 
-
                 <List notes={notes} />
-
             </header>
         </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,53 +1,53 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import App from "./App";
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
-test("renders the heading", () => {
+test('renders the heading', () => {
     render(<App />);
     const heading = screen.getByText(/Todo List & Notes/i);
     expect(heading).toBeInTheDocument();
 });
 
-test("renders the list of notes", () => {
+test('renders the list of notes', () => {
     render(<App />);
-    const list = document.getElementsByClassName("noteList");
+    const list = document.getElementsByClassName('noteList');
     expect(list).toBeTruthy();
 });
 
-test("renders the sample note", () => {
+test('renders the sample note', () => {
     render(<App />);
-    const noteList = document.getElementsByTagName("li");
-    expect(noteList[0]).toHaveTextContent("Start my to-do list");
+    const noteList = document.getElementsByTagName('li');
+    expect(noteList[0]).toHaveTextContent('Start my to-do list');
 });
 
-test("contains the new note input", () => {
+test('contains the new note input', () => {
     render(<App />);
-    const newNoteInput = document.getElementById("newNoteInput");
+    const newNoteInput = document.getElementById('newNoteInput');
     expect(newNoteInput).toBeEnabled();
 });
 
-test("allows the user to type a new note", async () => {
+test('allows the user to type a new note', async () => {
     render(<App />);
-    const newNoteInput = document.getElementById("newNoteInput");
+    const newNoteInput = document.getElementById('newNoteInput');
 
-    userEvent.type(newNoteInput, "hello world");
-    expect(newNoteInput.value).toBe("hello world");
+    userEvent.type(newNoteInput, 'hello world');
+    expect(newNoteInput.value).toBe('hello world');
 
-    fireEvent.submit(document.getElementById("newNoteForm"));
+    fireEvent.submit(document.getElementById('newNoteForm'));
 
-    const noteList = document.getElementsByTagName("li");
+    const noteList = document.getElementsByTagName('li');
 
-    expect(noteList[1]).toHaveTextContent("hello world");
+    expect(noteList[1]).toHaveTextContent('hello world');
 });
 
-test("saves the new note in the note list when the form is submitted", async () => {
+test('saves the new note in the note list when the form is submitted', async () => {
     render(<App />);
-    const newNoteInput = document.getElementById("newNoteInput");
+    const newNoteInput = document.getElementById('newNoteInput');
 
-    userEvent.type(newNoteInput, "hello world");
-    fireEvent.submit(document.getElementById("newNoteForm"));
+    userEvent.type(newNoteInput, 'hello world');
+    fireEvent.submit(document.getElementById('newNoteForm'));
 
-    const noteList = document.getElementsByTagName("li");
+    const noteList = document.getElementsByTagName('li');
 
-    expect(noteList[1]).toHaveTextContent("hello world");
+    expect(noteList[1]).toHaveTextContent('hello world');
 });
