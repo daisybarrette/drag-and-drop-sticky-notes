e31867877685d762e731774f2c186886123a697b
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-04-08 12:28:11 -0400

    Improve delete button accessibility

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -20,9 +20,20 @@ function Note({ note, index, handleRemoveNote }) {
                 >
                     <button
                         className='removeNote'
+                        type='button'
+                        aria-label='Delete note'
                         onClick={() => handleRemoveNote(note.list, index)}
                     >
-                        {'x'}
+                        <svg
+                            width='15'
+                            height='15'
+                            viewBox='0 0 44 44'
+                            aria-hidden='true'
+                            focusable='false'
+                        >
+                            <path d='M0.549989 4.44999L4.44999 0.549988L43.45 39.55L39.55 43.45L0.549989 4.44999Z' />
+                            <path d='M39.55 0.549988L43.45 4.44999L4.44999 43.45L0.549988 39.55L39.55 0.549988Z' />
+                        </svg>
                     </button>
                     <div className='note'>{`${note.content}`}</div>
                 </li>
