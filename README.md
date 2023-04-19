f01327d803b00d9912b411101c01821efdeceda5
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-04-19 16:52:25 -0400

    Address strict mode issue

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 A to-do app that allows users to drag and drop sticky notes into different columns. Built with [react-beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd).
 
-
 You can see it here: [https://drag-and-drop-sticky-notes.netlify.app/](https://drag-and-drop-sticky-notes.netlify.app/).
+
+## Local development
+
+`npm start` to load the app on `localhost:3000`
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Removing strict mode because react-beautiful-dnd has an issue rendering Droppables
+// https://github.com/atlassian/react-beautiful-dnd/issues/2399
+
 root.render(
-  <React.StrictMode>
+    //   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    //   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
